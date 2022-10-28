2838ea00087dc788d5a87cc711165502be184426
tum-yums <nhat.thomas@gmail.com>
2022-10-28 13:59:51 -0500

    Robot Constants
    I've created constants for the front and back motors. I am unsure of which specific parts of the robot you want me to make constants for, i'm going to go ahead and commit my code to git hub so you atleast have that and can take it from where I left off while I'm at the math competition and doing E&M make up test. I'm sorry for the inconvenience and thank you.

---
+++ TeamCode/src/main/java/Hardware/RobotConstants.java
@@ -0,0 +1,11 @@
+package Hardware;
+
+public class RobotConstants {
+    //constants for the wheelbase
+    public static final String LEFT_FRONT_MOTOR = "leftFront";
+    public static final String RIGHT_FRONT_MOTOR_ = "rightFront";
+    public static final String LEFT_BACK_MOTOR = "leftBack";
+    public static final String RIGHT_BACK_MOTOR = "rightBack";
+
+
+}
