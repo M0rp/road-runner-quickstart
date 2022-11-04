d034b9869a62ea2a66594b60f413ec05e2150364
tum-yums <nhat.thomas@gmail.com>
2022-11-04 16:48:40 -0500

    Update RobotConstants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/RobotConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/RobotConstants.java
@@ -1,9 +1,16 @@
 package org.firstinspires.ftc.teamcode.Hardware;
-
 public class RobotConstants {
     //constants for the wheelbase
     public static final String LEFT_FRONT_MOTOR = "leftFront";
-    public static final String RIGHT_FRONT_MOTOR = "rightFront";
+    public static final String RIGHT_FRONT_MOTOR_ = "rightFront";
     public static final String LEFT_BACK_MOTOR = "leftBack";
     public static final String RIGHT_BACK_MOTOR = "rightBack";
+
+    //constants for slide motors
+    public static final String FRONT_SLIDE_MOTOR = "frontSlide";
+    public static final String BACK_SLIDE_MOTOR = "backSlide";
+
+    //constants for the servos
+    public static final String SERVO_MOUNT = "servoMount";
+    public static final String SERVO_CLAW = "servoClaw";
 }
